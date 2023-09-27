580b8d5628476b2d0756b54a4cd0d8c4fc437674
rnfor-pro <nfor.rudolph1@gmail.com>
2023-09-27 05:11:11 +0000

    terraform cloud login

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "etech-dev"
+      workspaces {
+      name = "terra-house"
+    }
+  }
   required_providers {
     random = {
       source  = "hashicorp/random"
@@ -11,34 +17,50 @@ terraform {
   }
 }
 
+# provider "aws" {
+#   region = "AWS_DEFAULT_REGION"
+#   access_key = "AWS_ACCESS_KEY_ID"
+#   secret_key = "AWS_SECRET_ACCESS_KEY"
+# }
+
+# provider "random" {
+#   #configuration option
+# }
+
+
+# resource "random_string" "bucket_name" {
+#   lower   = true
+#   upper   = false
+#   numeric = false
+#   length  = 16
+#   special = false
+# }
+
+
+# #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+# resource "aws_s3_bucket" "example" {
+#   #Bucket naming rules
+#   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+#   bucket = random_string.bucket_name.result
+# }
+
+
+
+# #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+# output "random_bucket_name" {
+#   value = random_string.bucket_name.result
+# }
+
+# #kAKQYiySVtHxsA.atlasv1.Sx8S1nAiKgaDWqAFLJyrNuFTqDH5lz4rg1EXzKSFmdU7JPAUZ9HiKby84vpuozmfkm0
+
+
+
 provider "aws" {
-  #configuration option
+  # profile    = "default"
+  # region     = "us-east-1"
 }
 
-provider "random" {
-  #configuration option
-}
-
-
-resource "random_string" "bucket_name" {
-  lower   = true
-  upper   = false
-  numeric = false
-  length  = 16
-  special = false
-}
-
-
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
-  #Bucket naming rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = random_string.bucket_name.result
-}
-
-
-
-#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+resource "aws_instance" "example" {
+  ami           = "ami-0f3769c8d8429942f"
+  instance_type = "t2.micro"
 }
